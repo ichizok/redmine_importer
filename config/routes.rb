188dd01878feeb455235b8ab5c5d7275e575efb5
ichizok <gclient.gaap@gmail.com>
2015-03-04 14:59:02 +0900

    fix parameters of match method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-  match 'projects/:project_id/issues/import', :to => 'importer#index', :via => [:get, :post]
-  match 'projects/:project_id/issues/import/:action', :to => 'importer', :via => [:get, :post]
+  match 'projects/:project_id/issues/import', :controller => 'importer', :action => 'index', :via => [:get, :post]
+  match 'projects/:project_id/issues/import/:action', :controller => 'importer', :via => [:get, :post]
 end
